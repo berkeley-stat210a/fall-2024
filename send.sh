812543bde50398a54db3044bf8ba7120189a4dfa
Will Fithian <wfithian@gmail.com>
2025-06-10 14:48:11 -0700

    committed June 10

---
+++ send.sh
@@ -0,0 +1,5 @@
+cp ../fall-2024-private/homework/homework*.tex homework/.
+cp ../fall-2024-private/homework/homework*.pdf homework/.
+cp ../fall-2024-private/homework/*.csv homework/.
+cp ~/Dropbox/Notability/210A-F24-Lectures/lecture* handwritten
+quarto publish gh-pages
